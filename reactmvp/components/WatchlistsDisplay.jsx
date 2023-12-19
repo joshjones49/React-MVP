899076b5dad0d5a8160c566a6d921b74bb6b09de
joshjones49 <joshjones200285@gmail.com>
2023-12-19 01:29:10 -0600

    front end patch request functional

--- reactmvp/components/WatchlistsDisplay.jsx
+++ reactmvp/components/WatchlistsDisplay.jsx
@@ -1,9 +1,12 @@
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import toast, {Toaster} from 'react-hot-toast'
 
-const WatchlistsDisplay = ({createdLists, list, onDelete}) => {
+const WatchlistsDisplay = ({createdLists, list}) => {
   const [isDeleted, setIsDeleted] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(list.text);
+  const [editName, setEditName] = useState(list.name)
 
   const handleClick = () => {
     console.log(list.id)
@@ -55,19 +58,71 @@ const deleteList = async (e) => {
 
   const editList = (e) => {
     e.stopPropagation()
-    console.log(list.id)
+    setIsEditing(true)
+  }
+
+  const saveChanges = async (e) => {
+    e.stopPropagation()
+    setIsEditing(false)
+
+    let id = list.id
+
+    try {
+      const res = await fetch(`http://localhost:8000/watchlists/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({name: editName, text: editedText}),
+      });
+  
+      if (res.ok) {
+        console.log(`List with ID ${id} updated successfully`);
+      } else {
+        console.error(`Failed to update watchlist with ID ${id}`);
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
   }
 
   if(isDeleted) {
     return null;
   }
 
+  const handleOnChangeName = (e) => {
+    setEditName(e.target.value)
+    console.log(e.target.value)
+  }
+
+  const handleOnChangeText = (e) => {
+    setEditedText(e.target.value)
+    console.log(e.target.value)
+  }
+
   return (
       <div className="listDisplay" id={list.id} onClick={handleClick}>
-      <h1>{list.name}</h1>
-      <h4>{list.text}</h4>
+      <h1 
+      contentEditable={isEditing} 
+      id='listName'>{isEditing ? (
+        <input
+        className='editField' 
+        type='text' 
+        value={editName} 
+        onChange={handleOnChangeName} />
+      ) : (editName)}
+      </h1>
+      <h4 
+      contentEditable={isEditing} 
+        id='summary'>{isEditing ? (
+         <input
+         className='editField' 
+          type='text' 
+          value={editedText} 
+          onChange={handleOnChangeText} />
+      ) : editedText}</h4>
       <div id='edtdlt'>
-      <button onClick={editList} id='edit' className="edt">Edit</button>
+      <button onClick={isEditing ? saveChanges : editList} id='edit' className="edt">{isEditing ? 'Save' : 'Edit'}</button>
       <button onClick={deleteList} id='delete' className="edt">Delete</button>
       </div>
     </div>
