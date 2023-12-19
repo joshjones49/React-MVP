f2dc8b2d77e441db6b01b0a8e9e1541fb125d584
joshjones49 <joshjones200285@gmail.com>
2023-12-19 11:44:09 -0600

    `

--- reactmvp/components/Show.jsx
+++ reactmvp/components/Show.jsx
@@ -1,14 +1,18 @@
 
 
-function displayInfo(show) {
+
+const Show = ({show}) => {
+
+  function displayInfo() {
     console.log(show.summary)
 }
 
-const Show = ({show}) => {
+
   return (
     <div className="showDisplay" id={show.id} onClick={() => displayInfo(show)}>
-      <h1>{show.name}</h1>
-      <h4>IMAGE COMING</h4>
+      <img src={show.image}/>
+      <h1 id='showName' >{show.name}</h1>
+      <h4 id='showSummary' >{show.summary}</h4>
     </div>
   )
 }
--- reactmvp/src/migration.sql
+++ reactmvp/src/migration.sql
@@ -15,12 +15,12 @@ CREATE TABLE show (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     summary VARCHAR(1000),
-    image BYTEA
+    image VARCHAR(255)
 );
 
 INSERT INTO show (name, summary, image) VALUES 
-('BERSERK 1997', 'Berserk is a dark fantasy manga and anime series that follows the story of a mercenary named Guts who is seeking revenge against his former friend and leader, Griffith, who betrayed and sacrificed their comrades to become a powerful demon.', NULL),
-('KONOSUBA', 'Following an untimely and embarrassing death, Kazuma Satou, a Japanese teenage shut-in NEET, meets a goddess named Aqua, who offers to reincarnate him in a parallel world with MMORPG elements, where he can go on adventures and battle monsters.', NULL),
+('BERSERK 1997', 'Berserk is a dark fantasy manga and anime series that follows the story of a mercenary named Guts who is seeking revenge against his former friend and leader, Griffith, who betrayed and sacrificed their comrades to become a powerful demon.', 'https://github.com/joshjones49/React-MVP/blob/main/reactmvp/src/images/berserk-image.jpg?raw=true'),
+('KONOSUBA', 'Following an untimely and embarrassing death, Kazuma Satou, a Japanese teenage shut-in NEET, meets a goddess named Aqua, who offers to reincarnate him in a parallel world with MMORPG elements, where he can go on adventures and battle monsters.', 'https://github.com/joshjones49/React-MVP/blob/main/reactmvp/src/images/konosuba-image.jpg?raw=true'),
 ('KABANERI OF THE IRON FORTRESS', 'A large group of survivors seek shelter while travelling on a train after their station was attacked by aggressive, undead creatures called Kabane. As the world is in the middle of an industrial revolution, a monster appears that cannot be defeated unless its heart, which is protected by a layer of iron, is pierced.', NULL),
 ('BERSERK OF GLUTTONY', 'Born into a world with two kinds of people—those with superior skills and those without—he has been shunned his entire life. Fate has the skill of Gluttony, a curse that leaves him constantly hungry… until he awakens its terrible potential. When he kills someone, he can devour their skills and feed his insatiable appetite.', NULL),
 ('RURONI KENSHIN', 'Rurouni Kenshin tells the story of a wanderer named Kenshin, who used to be called Hitokiri Battousai during the Japanese civil war. His tale is about redemption of his past and all his murders by helping those in trouble with his reverse-blade sword, the sakabato.', NULL),
